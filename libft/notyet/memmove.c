fe9a292bf1139bcc982f3d3fd95240148124d804
Alxzxz <70357099+Alxzxz@users.noreply.github.com>
2024-10-07 22:02:57 +0200

    Update memmove.c

--- libft/notyet/memmove.c
+++ libft/notyet/memmove.c
@@ -1,2 +1,36 @@
 #include "libft.h"
 
+void	*ft_memmove(void *str1_dest, const void *str2_src, size_t n)
+{
+  unsigned char *dest = (unsigned char *)str1_dest;
+  const unsigned char *src = (const unsigned char *)str2_src;
+  size_t i;
+  
+  if (dest == src || n == 0) {
+    return str1_dest;
+  }
+  
+    if (src < dest && dest < src + n) {
+        i = n;
+        while (i > 0) {
+            i--;
+            dest[i] = src[i];
+        }
+    }
+    else {
+      return (memcpy(dest, src, n));
+    }
+
+  return ((void *)dest);  
+
+}
+
+// int main() {
+//     char str[] = "Hello World";
+
+//     // Copiar los primeros 5 caracteres a partir de la posición 6
+//     ft_memmove(str + 6, str, 5);
+
+//     printf("%s\n", str);  // Salida: Hello Hello
+//     return 0;
+// }
